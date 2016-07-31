605e42f9d250e6b10aef347298162392e784d231
AlissonRTeles <alisson.teles0@gmail.com>
2016-07-31 20:47:48 -0300

    Adicionados operadores
    Adicionados operadores básicos e  links para estudos após curso

--- sqlum.sql
+++ sqlum.sql
@@ -2,3 +2,40 @@
 -- Autor: Alissn R. Teles
 -- Data : 1 de agosto de 2016
 -- Descricao: Esse arquivo contem nocoes basicas de sql 
+
+-- ALGEBRA RELACIONAL
+-- https://pt.wikipedia.org/wiki/Álgebra_relacional
+
+-- PROJECAO
+-- https://pt.wikipedia.org/wiki/Projeção_(álgebra_relacional)
+
+-- SELECAO
+-- https://pt.wikipedia.org/wiki/Seleção_(álgebra_relacional)
+
+-- COMANDO BÁSICO DE SQL
+SELECT /*PROJECAO*/ FROM /*TABELA*/ WHERE /*SELECAO*/
+
+-- TIPOS DE OPERADORES BÁSICOS
+-- https://msdn.microsoft.com/pt-br/library/ms174986(v=sql.120).aspx
+
+-- ARITMETICOS
+-- https://msdn.microsoft.com/pt-br/library/ms187716(v=sql.120).aspx
+
+-- LOGICOS 
+-- https://msdn.microsoft.com/pt-br/library/ms189773(v=sql.120).aspx
+
+-- COMPARATIVOS
+-- https://msdn.microsoft.com/pt-br/library/ms188074(v=sql.120).aspx
+
+-- CLAUSULAS JOIN
+-- http://www.devmedia.com.br/clausulas-inner-join-left-join-e-right-join-associacao-de-tabelas-sql-server-2008-parte-2/18930
+-- acompanhar imagem no slide ou baixar no git
+
+-- SUBQUERIES 
+-- http://www.devmedia.com.br/t-sql-subqueries-onde-e-quando-utilizar/32072
+
+-- AGRUPAMENTOS
+-- http://www.devmedia.com.br/introducao-ao-sql-agrupamento-de-resultados/17008
+
+-- ORDENACAO
+-- https://msdn.microsoft.com/pt-br/library/ms188385.aspx
